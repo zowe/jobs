40188a07dc7ec57f01b1fc1e109f8bbb27e2a7d8
MarkAckert <Mark.Ackert@broadcom.com>
2019-01-23 11:24:45 -0500

    Setup gradlew bootstrap; remove gradle-wrapper jar
    Signed-off-by: MarkAckert <Mark.Ackert@broadcom.com>

---
+++ bootstrap_gradlew.bat
@@ -0,0 +1,15 @@
+@echo off
+
+if not exist gradle/wrapper/gradle-wrapper.jar (
+    echo Gradle Wrapper not found. Attempting to download...
+    powershell -Command "& {wget https://raw.githubusercontent.com/gradle/gradle/master/gradle/wrapper/gradle-wrapper.jar -OutFile gradle/wrapper/gradle-wrapper.jar}"
+    IF %ERRORLEVEL% NEQ 0  (
+        echo Gradle wrapper download failed. Bootstrap failed. 
+    ) ELSE (
+        echo Gradle wrapper download success; bootstrap complete.
+    )
+) else (
+    echo Gradle Wrapper found; bootstrap complete.
+)
+
+rem  powershell -Command <wget https://raw.githubusercontent.com/gradle/gradle/master/gradle/wrapper/gradle-wrapper.jar -OutFile gradle-wrapper.jar>
