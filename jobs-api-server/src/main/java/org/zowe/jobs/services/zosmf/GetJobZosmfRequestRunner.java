56171506825d241befeee99a282676ef2a57ed58
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2019-10-24 10:57:06 -0400

    some new code?
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

--- jobs-api-server/src/main/java/org/zowe/jobs/services/zosmf/GetJobZosmfRequestRunner.java
+++ jobs-api-server/src/main/java/org/zowe/jobs/services/zosmf/GetJobZosmfRequestRunner.java
@@ -49,6 +49,16 @@ public class GetJobZosmfRequestRunner extends AbstractZosmfJobsRequestRunner<Job
         return getJobFromJson(responseCache.getEntityAsJsonObject());
     }
 
+    protected void doSomething() {
+        System.out.println("Hello World 1!");
+        System.out.println("Hello World 2!");
+        System.out.println("Hello World 3!");
+        System.out.println("Hello World 4!");
+        System.out.println("Hello World 5!");
+        System.out.println("Hello World 6!");
+        System.out.println("Hello World 7!");
+    }
+
     @Override
     protected ZoweApiRestException createException(JsonObject jsonResponse, int statusCode) {
         return createJobNotFoundExceptions(jsonResponse, statusCode, jobName, jobId);
