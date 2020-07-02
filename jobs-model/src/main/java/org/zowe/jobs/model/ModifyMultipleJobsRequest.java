9c075577cc7d053d63d18f561d06876fc07f178d
Jordan Cain <jordan.cain1@uk.ibm.com>
2020-07-02 14:03:35 -0400

    fix sonarScan issues
    Signed-off-by: Jordan Cain <jordan.cain1@uk.ibm.com>

--- jobs-model/src/main/java/org/zowe/jobs/model/ModifyMultipleJobsRequest.java
+++ jobs-model/src/main/java/org/zowe/jobs/model/ModifyMultipleJobsRequest.java
@@ -2,13 +2,11 @@ package org.zowe.jobs.model;
 
 import java.util.ArrayList;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class ModifyMultipleJobsRequest extends ModifyJobRequest {
     private ArrayList<SimpleJob> jobs;
     
