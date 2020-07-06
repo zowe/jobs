79e5e705a8e8757efbe3d6eefa3ee6fb4fc1129e
Jordan Cain <jordan.cain1@uk.ibm.com>
2020-07-06 13:20:09 -0400

    Fix modifyJobsRequest example rendering in swagger
    Signed-off-by: Jordan Cain <jordan.cain1@uk.ibm.com>

--- jobs-model/src/main/java/org/zowe/jobs/model/ModifyMultipleJobsRequest.java
+++ jobs-model/src/main/java/org/zowe/jobs/model/ModifyMultipleJobsRequest.java
@@ -13,6 +13,6 @@ import lombok.NoArgsConstructor;
 public class ModifyMultipleJobsRequest {
     @ApiModelProperty(value = "The modify command, e.g. cancel, hold, release", dataType = "string", required = true, example = "cancel")
     private String command;
-    @ApiModelProperty(value = "The list of jobs to receive the modify command", dataType = "List", required = true, example = "[{jobId:job1234, jobName:TestJob}]")
+    @ApiModelProperty(value = "The list of jobs to receive the modify command", dataType = "string", required = true, example = "[{\"jobId\":\"job1234\", \"jobName\":\"TestJob\"}]")
     private ArrayList<SimpleJob> jobs;
 }
