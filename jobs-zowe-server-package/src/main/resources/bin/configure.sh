49761688e164a8a55bc4975c134183c30e3832cd
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2020-12-14 22:14:24 -0500

    let zowe launch script to handle configure
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

--- jobs-zowe-server-package/src/main/resources/bin/configure.sh
+++ jobs-zowe-server-package/src/main/resources/bin/configure.sh
@@ -13,39 +13,3 @@
 # Source main utils script
 . ${ROOT_DIR}/bin/utils/utils.sh
 ensure_java_is_on_path
-
-# Add static definition for jobs-api
-cat <<EOF >${STATIC_DEF_CONFIG_DIR}/jobs-api.ebcidic.yml
-#
-services:
-  - serviceId: jobs
-    title: IBM z/OS Jobs
-    description: IBM z/OS Jobs REST API service
-    catalogUiTileId: jobs
-    instanceBaseUrls:
-      - https://${ZOWE_EXPLORER_HOST}:${JOBS_API_PORT}/
-    homePageRelativeUrl:
-    routedServices:
-      - gatewayUrl: api/v1
-        serviceRelativeUrl: api/v1/jobs
-      - gatewayUrl: api/v2
-        serviceRelativeUrl: api/v2/jobs
-    apiInfo:
-      - apiId: com.ibm.jobs.v1
-        gatewayUrl: api/v1
-        version: 1.0.0
-        swaggerUrl: https://${ZOWE_EXPLORER_HOST}:${JOBS_API_PORT}/v2/api-docs
-        documentationUrl: https://${ZOWE_EXPLORER_HOST}:${JOBS_API_PORT}/swagger-ui.html
-      - apiId: com.ibm.jobs.v2
-        gatewayUrl: api/v2
-        version: 2.0.0
-        swaggerUrl: https://${ZOWE_EXPLORER_HOST}:${JOBS_API_PORT}/v2/api-docs
-        documentationUrl: https://${ZOWE_EXPLORER_HOST}:${JOBS_API_PORT}/swagger-ui.html
-catalogUiTiles:
-  jobs:
-    title: z/OS Jobs services
-    description: IBM z/OS Jobs REST services
-EOF
-iconv -f IBM-1047 -t IBM-850 ${STATIC_DEF_CONFIG_DIR}/jobs-api.ebcidic.yml > $STATIC_DEF_CONFIG_DIR/jobs-api.yml
-rm ${STATIC_DEF_CONFIG_DIR}/jobs-api.ebcidic.yml
-chmod 770 $STATIC_DEF_CONFIG_DIR/jobs-api.yml
