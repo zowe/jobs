68319af26915ffe73af12c5a51e45ff4e2a6e753
Carson Cook <carson.cook@ibm.com>
2022-04-21 14:32:16 -0400

    Fix if in start.sh
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

--- jobs-zowe-server-package/src/main/resources/bin/start.sh
+++ jobs-zowe-server-package/src/main/resources/bin/start.sh
@@ -27,11 +27,10 @@
 # - ZWE_GATEWAY_HOST
 # - ZWE_haInstance_hostname
 # - ZWE_zowe_certificate_keystore_type - The default keystore type to use for SSL certificates
-echo "$(pwd)"
 JAR_FILE=$(ls -1 $(pwd)/bin/jobs-api-server-*.jar | head -n 1)
 
 LOG_LEVEL=
-if [[ ! -z ${ZWE_configs_debug} && ${ZWE_configs_debug} == true ]]
+if [ "${ZWE_configs_debug}" = true ]
 then
   LOG_LEVEL="debug"
 fi
