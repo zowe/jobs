010fc9e1a53553420a7fdc936b05486960dfde6d
Carson Cook <carson.cook@ibm.com>
2022-04-21 13:37:55 -0400

    Fix use of start.sh for v2
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

--- jobs-zowe-server-package/src/main/resources/bin/start.sh
+++ jobs-zowe-server-package/src/main/resources/bin/start.sh
@@ -30,6 +30,12 @@
 
 JAR_FILE=$(ls -1 $(pwd)/bin/jobs-api-server-*.jar | head -n 1)
 
+LOG_LEVEL=
+if [[ ! -z ${ZWE_configs_debug} && ${ZWE_configs_debug} == true ]]
+then
+  LOG_LEVEL="debug"
+fi
+
 options="-Xms16m -Xmx512m"
 if [ `uname` = "OS/390" ]; then
   options="${options} -Xquickstart"
@@ -41,17 +47,12 @@ _BPX_JOBNAME=${ZWE_zowe_job_prefix}${COMPONENT_CODE} java \
   -Dibm.serversocket.recover=true \
   -Dfile.encoding=UTF-8 \
   -Djava.io.tmpdir=${TMPDIR:-${TMP:-/tmp}} \
+  -Dspring.profiles.include="${LOG_LEVEL}" \
   -Dserver.port=${ZWE_configs_port:-8545} \
   -Dserver.ssl.keyAlias="${ZWE_configs_certificate_keystore_alias:-${ZWE_zowe_certificate_keystore_alias}}" \
   -Dserver.ssl.keyStore="${ZWE_configs_certificate_keystore_file:-${ZWE_zowe_certificate_keystore_file}}" \
   -Dserver.ssl.keyStorePassword="${ZWE_configs_certificate_keystore_password:-${ZWE_zowe_certificate_keystore_password}}" \
   -Dserver.ssl.keyStoreType="${ZWE_configs_certificate_keystore_type:-${ZWE_zowe_certificate_keystore_type:-PKCS12}}" \
-#  -Dspring.profiles.include="${EXPLORER_API_LOG_LEVEL}" \
-#  -Dserver.port=${JOBS_API_PORT:-8545} \
-#  -Dserver.ssl.keyAlias="${KEY_ALIAS}" \
-#  -Dserver.ssl.keyStore="${KEYSTORE}" \
-#  -Dserver.ssl.keyStorePassword="${KEYSTORE_PASSWORD}" \
-#  -Dserver.ssl.keyStoreType=${KEYSTORE_TYPE:-PKCS12} \
   -Dserver.connection-timeout=8000 \
   -Dcom.ibm.jsse2.overrideDefaultTLS=true \
   -Dconnection.httpsPort=${ZWE_components_gateway_port:-7554} \
