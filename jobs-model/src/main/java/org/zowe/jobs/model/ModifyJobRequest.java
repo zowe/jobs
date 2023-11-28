9d645f291b14aff4f782a33bd3876d7f1372d012
Pavel Jareš <pavel.jares@broadcom.com>
2023-11-28 13:36:50 +0100

    fix licenses

--- jobs-model/src/main/java/org/zowe/jobs/model/ModifyJobRequest.java
+++ jobs-model/src/main/java/org/zowe/jobs/model/ModifyJobRequest.java
@@ -7,6 +7,7 @@
  *
  * Copyright IBM Corporation 2019
  */
+
 package org.zowe.jobs.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
--- jobs-tests/src/test/java/org/zowe/jobs/tests/JobsLogoutIntegrationTest.java
+++ jobs-tests/src/test/java/org/zowe/jobs/tests/JobsLogoutIntegrationTest.java
@@ -7,6 +7,7 @@
  *
  * Copyright IBM Corporation 2019, 2020
  */
+
 package org.zowe.jobs.tests;
 
 import io.restassured.RestAssured;
